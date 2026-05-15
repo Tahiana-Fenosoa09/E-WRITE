7c4657a6141489c305ddcc621a0d2134db26af8e
Tahiana-Fenosoa <rasamytahiana@gmail.com>
2026-05-15 08:06:32 +0300

    added most interactity with JS,created the tutorial section, and made the navigation for SPA system

---
+++ front/interactivity/functionActivator.js
@@ -0,0 +1,13 @@
+import navigation  from "./navigation.js";
+import showLearnMore from "./learn-more.js";
+import slideNavBar from "./slideNavigationBar.js";
+import tutorials from "./tutorials.js";
+
+function functionActivator(){
+    navigation();
+    showLearnMore();
+    slideNavBar();
+    tutorials();
+}
+
+functionActivator();
